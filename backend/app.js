81c1c6489fa6214eb954252e92cce1ddfd929ef5
brayann graux <pics.prod45@gmail.com>
2024-08-30 17:12:53 +0200

    first commit backend

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+// app.js
+const express = require('express');
+const path = require("path")
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user'); // Importer les routes d'utilisateur
+const stuffRoutes = require('../backend/routes/stuff');
+
+const app = express();
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(express.json()); // Pour parser le JSON des requêtes
+
+// Middleware CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  next();
+});
+
+// MONGOOSE CONNECT
+mongoose.connect('mongodb+srv://grauxbrayann:Bg10061997@cluster0.st5fa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.log('Connexion à MongoDB échouée !', error));
+
+// Utiliser les routes d'utilisateur
+app.use('/api/auth', userRoutes);
+app.use("/api/books", stuffRoutes);
+
+module.exports = app;
---
+++ backend/controllers/stuff.js
@@ -0,0 +1,92 @@
+const Book = require('../models/Book');
+const fs = require('fs');
+
+exports.createBook = (req, res, next) => {
+    try {
+      const bookObject = JSON.parse(req.body.book);
+      delete bookObject._id;
+      delete bookObject._userId;
+      const { title, author, year, genre } = bookObject;
+      const book = new Book({
+        ...bookObject,
+        userId: req.auth.userId,
+        title: title,
+        author: author,
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+        year: year,
+        genre: genre,
+      });
+      book
+        .save()
+        .then(() => {
+          console.log("Livre créé avec succès");
+          res.status(201).json({ message: "Livre créé avec succès !" });
+        })
+        .catch((error) => {
+          console.error("Erreur lors de la sauvegarde du livre :", error);
+          res.status(400).json({ error });
+        });
+    } catch (error) {
+      console.error("Erreur dans createBook :", error);
+      res.status(400).json({ error: "Erreur lors de la création du livre" });
+    }
+  };
+
+exports.modifyBook = (req, res, next) => {
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        inmageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {...req.body };
+
+    delete bookObject._userId;
+    Book.findOne({_id: req.params.id})
+        .then((book ) => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({  message: 'Non autorisé !'});
+            } else {
+                Book.updateOne({_id: req.params.id}, {...bookObject, _id: req.params.id})
+                .then(() => res.status(200).json({message: 'Objet modifié !'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch(error => res.status(400).json({ error }));
+};
+
+// exports.deleteBook = (req, res, next) => {
+//     Book.findOne({_id: req.params.id})
+//     .then(book => {
+//         if (book.userId != req.auth.userId) {
+//             res.status(401).json({message: 'Non autorisé !'});
+//         } else {
+//             const filname = book.imageUrl.split('/images') [1];
+//             fs.unlink(`images/${filename}`, () => {
+//                 Book.deleteOne({_id: req.params.id})
+//                 .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+//                 .catch(error => res.status(401).json({error}));
+//             });
+//         }
+        
+//     } )
+//     .catch(error => {
+//         res.status(500).json({error});
+//     })
+// };
+
+exports.getOneBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .then(books => res.status(200).json(books))
+        .catch(error => res.status(404).json({ error }));
+};
+
+exports.getAllBooks = async (req, res) => {
+    try {
+      const books = await Book.find().catch((error) =>
+        res.status(400).json({ error })
+      ); // si le serveur ne comprend pas la requete
+      res.json(books); // Les livres sont envoyés au client sous forme de réponse JSON
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+};
