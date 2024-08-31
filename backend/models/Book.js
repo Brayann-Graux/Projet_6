eb337c0f5fa91d35916d0884366ec1b36e12f49d
brayann graux <pics.prod45@gmail.com>
2024-08-31 02:11:56 +0200

    connexion & inscription ok , ajout book ok

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -6,12 +6,7 @@ const bookSchema = mongoose.Schema({
     imageUrl: { type: String, require: true },
     year: { type: Number, require: true },
     genre: { type: String, require: true },
-    // ratings: [
-    //     {
-    //         grade: { type: Number, require: true },
-    //     }
-    // ],
-    // averageRating: { type: Number, require: true },
+    
 });
 
 module.exports = mongoose.model('Book', bookSchema);
